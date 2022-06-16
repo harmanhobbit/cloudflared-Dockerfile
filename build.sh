ea6bd08d99cf1ebbb613a676b6cb8143897555b8
Ian Allaway <66171552+harmanhobbit@users.noreply.github.com>
2022-06-16 14:12:04 +0100

    Quick fix

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 echo $version
 source ./buildvars 
-
-echo ${{ github.repository_owner }}
-
-IMAGE_ID=ghcr.io/${{ github.repository_owner }}/$IMAGE_NAME 
+IMAGE_ID=ghcr.io/harmanhobbit/$IMAGE_NAME 
 IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]') 
 
 echo $IMAGE_ID

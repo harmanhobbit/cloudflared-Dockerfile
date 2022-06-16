2604ea1af58cc2dbb5dae2e7b7ad4bc721109e96
Ian Allaway <66171552+harmanhobbit@users.noreply.github.com>
2022-06-16 14:13:02 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ echo $IMAGE_ID
 
 for n in "${!domains[@]}" 
 do 
-  docker build --no-cache --build-arg CLOUDFLARED_VERSION=$version --build-arg DOMAIN=${domains[$n]} \
-    -t $IMAGE_NAME-${domains[$n]}:$version \ 
-    -t $IMAGE_NAME-${domains[$n]}:latest . 
+  docker build --no-cache --build-arg CLOUDFLARED_VERSION=$version --build-arg DOMAIN=${domains[$n]} -t $IMAGE_NAME-${domains[$n]}:$version -t $IMAGE_NAME-${domains[$n]}:latest . 
   echo IMAGE_ID=$IMAGE_ID 
   echo version=$version 
   docker push --all-tags $IMAGE_ID-${domains[$n]}

f14abf6313e3d51c0dd3bdb7cbbcf76dc2b1ef7a
Ian Allaway <66171552+harmanhobbit@users.noreply.github.com>
2022-06-16 13:58:22 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo $version
+source ./buildvars 
+IMAGE_ID=ghcr.io/${{ github.repository_owner }}/$IMAGE_NAME 
+IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]') 
+for n in "${!domains[@]}" 
+do 
+  docker build --no-cache --build-arg CLOUDFLARED_VERSION=$version --build-arg DOMAIN=${domains[$n]} \
+    -t $IMAGE_NAME-${domains[$n]}:$version \ 
+    -t $IMAGE_NAME-${domains[$n]}:latest . 
+  echo IMAGE_ID=$IMAGE_ID 
+  echo version=$version 
+  docker push --all-tags $IMAGE_ID-${domains[$n]}
+done
